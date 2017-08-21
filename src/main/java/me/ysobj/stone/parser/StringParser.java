eeb2b088ace8919d6e0bac482ff1c8c3b3bf4ee0
ysobj <litailang+ysobj@gmail.com>
2017-08-22 08:33:19 +0900

    add StringParser

---
+++ src/main/java/me/ysobj/stone/parser/StringParser.java
@@ -0,0 +1,27 @@
+package me.ysobj.stone.parser;
+
+import me.ysobj.stone.exception.ParseException;
+import me.ysobj.stone.model.ASTNode;
+import me.ysobj.stone.model.StringLiteral;
+import me.ysobj.stone.model.Token;
+import me.ysobj.stone.model.Token.TokenType;
+import me.ysobj.stone.tokenizer.Tokenizer;
+
+public class StringParser extends KeywordParser {
+	@Override
+	public ASTNode parse(Tokenizer tokenizer) throws ParseException {
+		Token token = tokenizer.peek();
+		if (token == Token.EOF) {
+			throw new ParseException();
+		}
+		if (keyword == null && token.getType() == TokenType.STRING) {
+			tokenizer.next();
+			return new StringLiteral(token);
+		}
+		throw new ParseException();
+	}
+	@Override
+	public String toString() {
+		return keyword != null ? keyword : "STRING";
+	}
+}
